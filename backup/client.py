ce35f3386ac74ece1efb9f5d95dd493bf19306c6
jasonwang <1374980762@qq.com>
2022-03-20 13:42:28 +0800

    Feature: 改善左右切换问题.

--- backup/client.py
+++ backup/client.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "http://127.0.0.1:10001/"
-params = {"fun": "search", 'recipe': 'test'.encode('utf-8')}
+url = "http://127.0.0.1:9997/"
+params = {"search": "西红柿炒鸡蛋"}
 params2 = {'fun': 'switch_left'}
 params3 = {'video': 'ON'}
-res = requests.get(url=url, params=params3)
+res = requests.get(url=url, params=params2)
 print(res.text)
